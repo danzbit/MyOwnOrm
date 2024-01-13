cd719af6d747335eec37f30bdef68735ed8eefab
danzbit <121254217+danzbit@users.noreply.github.com>
2024-01-13 13:21:04 +0100

    changed structure after review

--- MyOwnORM/MyOwnORM/CustomDbContextOptionsBuilder.cs
+++ MyOwnORM/MyOwnORM/CustomDbContextOptionsBuilder.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MyOwnORM
 {
-    public class CustomDbContextOptionsBuilder : CustonSqlServerContextOptionsBuilderExtension
+    public class CustomDbContextOptionsBuilder 
     {
+        public static void UseSqlServer(string connectionString)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                Console.WriteLine("Connection opened!");
+            }
+        }
     }
 }
--- MyOwnORM/MyOwnORM/CustomDbSet.cs
+++ MyOwnORM/MyOwnORM/CustomDbSet.cs
@@ -7,6 +7,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using MyOwnORM.Repository;
 
 namespace MyOwnORM
 {
